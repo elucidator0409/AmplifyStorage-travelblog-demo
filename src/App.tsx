9278c7463de1e520fe8b36c9ce46ac4b4e031bfd
tony <tranquockhanhdhsp99@gmail.com>
2025-05-25 02:14:04 +0700

    travel blog init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+// import { Amplify } from 'aws-amplify';
+// import outputs from "../amplify_outputs.json";
+
+// Amplify.configure(outputs);
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
